e2608866ef908edc4e3d1bba7124f2d1708200a2
Rachmizard <rachmizard11072000@gmail.com>
2023-09-11 14:06:05 +0700

    feat: add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { AccountModule } from './account/account.module';
 import { CryptoModule } from './shared/crypto/crypto.module';
+import { DatabaseModule } from './database/database.module';
+import { AppConfigModule } from './shared/app-config/app-config.module';
 
 @Module({
-  imports: [AuthModule, AccountModule, CryptoModule],
+  imports: [
+    AuthModule,
+    AccountModule,
+    CryptoModule,
+    DatabaseModule,
+    AppConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
